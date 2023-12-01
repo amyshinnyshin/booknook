5e3b9cd9a286ae6104c4e1b57828dc314d929c92
amyshinnyshin@gmail.com <amyshinnyshin@gmail.com>
2023-12-01 14:48:27 -0600

    continuing to work on URL links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import LeftSidebar from './components/LeftSidebar/LeftSidebar';
 import HomePage from './components/HomePage';
@@ -8,6 +8,10 @@ import FavoritesPage from './components/FavoritesPage';
 import BookDetailsPage from './components/BookDetailsPage';
 import { FavoriteBooksProvider } from './components/FavoritesBookContext';
 
+function NotFoundPage() {
+  return <h1>404 - Not Found</h1>;
+}
+
 function App() {
   return (
     <Router>
@@ -15,10 +19,16 @@ function App() {
         <LeftSidebar />
         <div className="main-page-content-container">
           <Routes>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/browse" element={<BrowsePage />} />
-            <Route path="/browse/books/:id" element={<BookDetailsPage />} />
-            <Route path="/favorites" element={<FavoritesPage />} />
+            {/* Redirect root path to "/booknook/home" */}
+            <Route path="/" element={<Navigate to="/booknook/home" replace />} />
+            
+            <Route path="/booknook/home" element={<HomePage />} />
+            <Route path="/booknook/browse" element={<BrowsePage />} />
+            <Route path="/booknook/browse/books/:id" element={<BookDetailsPage />} />
+            <Route path="/booknook/favorites" element={<FavoritesPage />} />
+
+            {/* Catch-all route for 404 errors */}
+            <Route path="/*" element={<NotFoundPage />} />
           </Routes>
         </div>
       </FavoriteBooksProvider>
@@ -27,5 +37,6 @@ function App() {
 }
 
 export default App;
+
 
 
