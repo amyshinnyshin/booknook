2b07d14dd6a774c1e20b38b710a2a0224b76f42f
amyshinnyshin@gmail.com <amyshinnyshin@gmail.com>
2023-12-01 15:44:00 -0600

    changing BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import LeftSidebar from './components/LeftSidebar/LeftSidebar';
 import HomePage from './components/HomePage';
